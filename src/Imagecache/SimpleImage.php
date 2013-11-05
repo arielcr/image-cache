96e0a7dadb2b3f2e5292688aa75fccd8a7148e72
Ariel Orozco <aorozco@77digital.com>
2013-11-05 17:57:53 -0600

    Included simpleimage

--- src/Imagecache/SimpleImage.php
+++ src/Imagecache/SimpleImage.php
@@ -1,15 +1,107 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-   "http://www.w3.org/TR/html4/loose.dtd">
+<?php
 
-<html lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>Simpleimage</title>
-	<meta name="generator" content="TextMate http://macromates.com/">
-	<meta name="author" content="Ariel Orozco">
-	<!-- Date: 2013-11-05 -->
-</head>
-<body>
+/*
+ * File: SimpleImage.php
+ * Author: Simon Jarvis
+ * Copyright: 2006 Simon Jarvis
+ * Date: 08/11/06
+ * Link: http://www.white-hat-web-design.co.uk/articles/php-image-resizing.php *
+ *
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation; either version 2 
+ * of the License, or (at your option) any later version. 
+ *  
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * GNU General Public License for more details: 
+ * http://www.gnu.org/licenses/gpl.html 
+ * 
+ * */
 
-</body>
-</html>
+namespace Imagecache;
+
+class SimpleImage
+{
+    var $image;
+    var $image_type;
+
+    function load($filename)
+    {
+        $image_info = getimagesize($filename);
+        $this->image_type = $image_info[2];
+        if ($this->image_type == IMAGETYPE_JPEG) {
+            $this->image = imagecreatefromjpeg($filename);
+        } elseif ($this->image_type == IMAGETYPE_GIF) {
+            $this->image = imagecreatefromgif($filename);
+        } elseif ($this->image_type == IMAGETYPE_PNG) {
+            $this->image = imagecreatefrompng($filename);
+        }
+    }
+
+    function save($filename, $image_type = IMAGETYPE_JPEG, $compression = 75, $permissions = null)
+    {
+        if ($image_type == IMAGETYPE_JPEG) {
+            imagejpeg($this->image, $filename, $compression);
+        } elseif ($image_type == IMAGETYPE_GIF) {
+            imagegif($this->image, $filename);
+        } elseif ($image_type == IMAGETYPE_PNG) {
+            imagepng($this->image, $filename);
+        }
+        if ($permissions != null) {
+            chmod($filename, $permissions);
+        }
+    }
+
+    function output($image_type = IMAGETYPE_JPEG)
+    {
+        if ($image_type == IMAGETYPE_JPEG) {
+            imagejpeg($this->image);
+        } elseif ($image_type == IMAGETYPE_GIF) {
+            imagegif($this->image);
+        } elseif ($image_type == IMAGETYPE_PNG) {
+            imagepng($this->image);
+        }
+    }
+
+    function getWidth()
+    {
+        return imagesx($this->image);
+    }
+
+    function getHeight()
+    {
+        return imagesy($this->image);
+    }
+
+    function resizeToHeight($height)
+    {
+        $ratio = $height / $this->getHeight();
+        $width = $this->getWidth() * $ratio;
+        $this->resize($width, $height);
+    }
+
+    function resizeToWidth($width)
+    {
+        $ratio = $width / $this->getWidth();
+        $height = $this->getheight() * $ratio;
+        $this->resize($width, $height);
+    }
+
+    function scale($scale)
+    {
+        $width = $this->getWidth() * $scale / 100;
+        $height = $this->getheight() * $scale / 100;
+        $this->resize($width, $height);
+    }
+
+    function resize($width, $height)
+    {
+        $new_image = imagecreatetruecolor($width, $height);
+        imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
+        $this->image = $new_image;
+    }
+} 
+
+?>
