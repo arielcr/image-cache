77fbb123ddcdaaa98047e1214bcac34d7d872e37
Ariel Orozco <aorozco@77digital.com>
2013-11-05 17:13:12 -0600

    Check if image exists in directory

--- src/Imagecache/Image.php
+++ src/Imagecache/Image.php
@@ -9,36 +9,36 @@ class Image{
 
     public static function cache($image_url)
     {
-
-        //get the name of the file
+        // Get file name
         $exploded_image_url = explode("/", $image_url);
         $image_filename = end($exploded_image_url);
         $exploded_image_filename = explode(".", $image_filename);
         $extension = end($exploded_image_filename);
 
-
-        //make sure its an image
+        // Image validation
         if (strtolower($extension) == "gif" || strtolower($extension) == "jpg" || strtolower($extension) == "png") {
 
-            //get the remote image
-            //$image_to_fetch = file_get_contents($image_url);
-
+            // Get remote image
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $image_url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             $image_to_fetch = curl_exec($ch);
             curl_close($ch);
 
-            //save it
-            $local_image_file = fopen(self::$image_path . $image_filename, 'w+');
-            chmod(self::$image_path . $image_filename, 0755);
-            fwrite($local_image_file, $image_to_fetch);
-            fclose($local_image_file);
+			// Check if image exists
+			if (file_exists(self::$image_path . $image_filename)) {
+				return "YA EXISTE! : " . self::$image_path . $image_filename;
+			} else {
+				// Save image
+	            $local_image_file = fopen(self::$image_path . $image_filename, 'w+');
+	            chmod(self::$image_path . $image_filename, 0755);
+	            fwrite($local_image_file, $image_to_fetch);
+	            fclose($local_image_file);
+			}
 
         }
 
-
-        return self::$image_path . $image_filename;
+        return "NO EXISTE, GUARDADA: " . self::$image_path . $image_filename;
     }
 
 }
