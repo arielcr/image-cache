5a65cbf85c2245decb9bd49ec883534dbc1d33f0
Ariel Orozco <aorozco@77digital.com>
2013-11-06 17:34:21 -0600

    Added test class

---
+++ tests/CacheTest.php
@@ -0,0 +1,14 @@
+<?php
+class CacheTest extends PHPUnit_Framework_TestCase
+{
+    public function testEmpty()
+    {
+        $stack = array();
+        $this->assertEmpty($stack);
+ 
+        return $stack;
+    }
+
+}
+
+?>
