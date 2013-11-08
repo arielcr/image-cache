2a4c53d76a30a5d542350f71a8b2114aa094f76e
Ariel Orozco <aorozco@77digital.com>
2013-11-08 10:42:06 -0600

    Test Cases

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -1,13 +1,12 @@
 <?php
 class CacheTest extends PHPUnit_Framework_TestCase
 {
-    public function testEmpty()
-    {
-        $stack = array();
-        $this->assertEmpty($stack);
- 
-        return $stack;
-    }
+    public function testTrueIsTrue()
+	    {
+	        // test to pass
+	        $var = true;
+	        $this->assertTrue($var);
+	    }
 
 }
 
