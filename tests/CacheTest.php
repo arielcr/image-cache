fe385e7df368e5c4c42761274bbfb1366292e83c
Ariel Orozco <aorozco@77digital.com>
2013-11-08 15:21:13 -0600

    Modified Test Case

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -4,6 +4,7 @@ class CacheTest extends PHPUnit_Framework_TestCase
 {
     public function testCacheReturnsSavedImage()
     {
+		// Test Cache Class
         $expected = 'cache/images/test.jpg';
         $image = \Image\Cache::get('http://www.crecetu.com/web/12172807/fotos/test.jpg');
 
